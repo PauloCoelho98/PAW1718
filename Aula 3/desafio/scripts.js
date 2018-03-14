ac03eb1ddd3801b7ac2a4cbb3a5bf73b4333e14a
Paulo Coelho <paulocoelho.98@hotmail.com>
2018-03-14 16:44:02 +0000

    aula 3 desafio

---
+++ Aula 3/desafio/scripts.js
@@ -0,0 +1,23 @@
+function calculate(){
+
+    let peso = document.getElementById("customWeight").value;
+    let altura = document.getElementById("customHeight").value;
+
+    let imc = Math.floor(peso/(Math.pow(altura,2)));
+
+    if(imc<=18){ // imc1
+        document.getElementById('img').src="imc1.png";
+        alert("imc1");
+    }else if(imc>18 && imc<=25){ // imc2
+        document.getElementById('img').src="imc2.png";
+        alert("imc2");
+    }else if(imc>25 && imc<=30){ // imc3
+        document.getElementById('img').src="imc3.png";
+        alert("imc3");
+    }else{ // imc4
+        document.getElementById('img').src="imc4.png";
+        alert("imc4");
+    }
+    document.getElementById("button").addEventListener(calculate); //Equivalente a usar onclick
+
+}
